83015d1bc4d78285b131cfbf467ca309f1c98a1f
aponce25 <>
2021-04-03 05:39:04 +0000

    Practical Make-up Exam

--- main.c
+++ main.c
@@ -21,25 +21,37 @@ int main(void) {
     
     // TODO: Fix the code below.
     // HINT: The code doesn't need big changes. Review the conditions and the outputs carefully.
+
     int sysP, diasP;
 
+  
     // Read Systolic and Diastolic pressure
-    scanf("%d %d", &sysP, &sysP);
+
+    printf("Enter Systolic and Diastolic: ");
+    scanf("%d %d", &sysP, &diasP);
 
     if(sysP < 120 && diasP < 80) {
       printf("Normal\n");
-    } else if(sysP > 129 || diasP > 80) {
+
+    } else if((sysP >= 120 && sysP <= 129) && diasP < 80) {
       printf("Elevated\n");
-    } else if(sysP < 139 && diasP < 89) {
-      printf("Stage 1 Hypertension");
-    } else if(sysP > 140 && diasP > 90) {
-      printf("Stage 1 Hypertension");
-    } else {
-      printf("Hypertensive Crysis");
+
+    } else if((sysP >= 130 && sysP <= 139) || (diasP >= 80 && diasP <= 89)) {
+      printf("Stage 1 Hypertension\n");
+
+    } else if((sysP >= 140 && sysP < 180) || (diasP >= 90 && diasP < 120)) {
+      printf("Stage 2 Hypertension\n");
+
+    } else if(sysP >= 180 || diasP >= 120) {
+      printf("Hypertensive Crysis\n");
+
+    } else if(sysP < diasP && sysP > diasP) {
+      printf("ERROR\n");
     }
-    
+
     /* Problem 1 End */
-  } else if(opt == 2) {
+  } 
+  else if(opt == 2) {
     
     /***************************
      *  Problem 2: Pokemon Damage
@@ -47,7 +59,24 @@ int main(void) {
     
     // TODO: Your P2 code goes here
     // Good luck!!
+    int pikaCHU;
+    int charZRD;
+    int opt;
+    int lvL;
+    int atkNME
+    int poWR
     
+    printf("Enter Pokemon Name (Pikachu : 1 or Charizard : 2): ");
+    scanf("%d", &opt);
+
+    if(opt == 1) {
+      printf("Enter Pikachu Level (1 - 100): ");
+      
+
+    }else if(opt == 2) {
+      printf("Enter Charizard Level (1 - 100): ");
+    }
+
     /* Problem 2 End */
   } else if(opt == 3) {
     
